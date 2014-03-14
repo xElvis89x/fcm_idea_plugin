18feee01eb5f04981725480a0cdb6f239d014f69
Elvis <ekurtnebiev@oysterlabs.com>
2014-03-14 18:36:52 +0200

    Added some JGrapht component ot plugin

--- src/com/elvis/visualfsm/controller/FSMDesignerController.java
+++ src/com/elvis/visualfsm/controller/FSMDesignerController.java
@@ -3,6 +3,11 @@ package com.elvis.visualfsm.controller;
 import com.elvis.visualfsm.view.FSMDesignerForm;
 import com.elvis.visualfsm.view.FragmentView;
 import com.intellij.openapi.project.Project;
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -28,15 +33,32 @@ public class FSMDesignerController {
         view.getAddFragmentButton().addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                FragmentView fragmentView = new FragmentView();
-                view.getDesignerPanel().setLayout(null);
-                fragmentView.getContentPane().setLocation(20, 20);
-                view.getDesignerPanel().add(fragmentView.getContentPane());
+//                FragmentView fragmentView = new FragmentView();
+//                view.getDesignerPanel().setLayout(null);
+//                fragmentView.getContentPane().setLocation(20, 20);
+//                view.getDesignerPanel().add(fragmentView.getContentPane());
 
 
             }
         });
 
+//        DirectedGraph<String, DefaultEdge> g = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+//        g.addEdge("asdfasd", "asdasd");
+//        g.addEdge("asdfasd1", "asdasd1");
+//        g.addEdge("asdfasd", "asasdasddasd1");
+
+        mxGraph graph = new mxGraph();
+        graph.getModel().beginUpdate();
+
+        Object v1 = graph.insertVertex(graph.getDefaultParent(), null, "Hello", 20, 20, 80, 30);
+        Object v2 = graph.insertVertex(graph.getDefaultParent(), null, "World!", 240, 150, 80, 30);
+        graph.insertEdge(graph.getDefaultParent(), null, "Edge", v1, v2);
+
+        graph.getModel().endUpdate();
+        mxGraphComponent mxGraphComponent = new mxGraphComponent(graph);
+        view.getDesignerPanel().setLayout(new BoxLayout(view.getDesignerPanel(),BoxLayout.LINE_AXIS));
+
+        view.getDesignerPanel().add(mxGraphComponent.getGraphControl());
 
     }
 }
